8bcc87402be0d63cfcf099b1f50ba97324319ea1
kusayia <132732228+kusayia@users.noreply.github.com>
2023-05-19 11:57:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,14 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#1. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+chmod -R 755 files
+# sed -i '$i uci set network.wan.ifname='"'"'eth3'"'"'' package/lean/default-settings/files/zzz-default-settings
+# sed -i '$i uci set network.wan6.ifname='"'"'eth3'"'"'' package/lean/default-settings/files/zzz-default-settings
+# sed -i '$i uci set network.wan.proto='"'"'pppoe'"'"'' package/lean/default-settings/files/zzz-default-settings
+# sed -i '$i uci set network.wan.username='"'"'account'"'"'' package/lean/default-settings/files/zzz-default-settings
+# sed -i '$i uci set network.wan.password='"'"'passwd'"'"'' package/lean/default-settings/files/zzz-default-settings
+# sed -i '$i uci set network.lan.ifname='"'"'eth0 eth1 eth2'"'"'' package/lean/default-settings/files/zzz-default-settings
+# sed -i '$i uci commit network' package/lean/default-settings/files/zzz-default-settings
